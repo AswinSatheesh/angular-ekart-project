562db6bdd571adab1f52c4023b31af5b167ca3c8
Aswin Satheesh <aswinsatheesh2001@gmail.com>
2024-06-12 22:19:34 +0530

    custom Diective

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { ProductComponent } from './container/product-list/product/product.compo
 import { FilterComponent } from './container/product-list/filter/filter.component';
 import { ProductDetailComponent } from './container/product-detail/product-detail.component';
 import { FeaturedBrandsComponent } from './container/featured-brands/featured-brands.component';
-
+import {SetBackground} from './CustomDirectives/setBackground.directive'
 @NgModule({
   declarations: [
     AppComponent,
@@ -28,7 +28,8 @@ import { FeaturedBrandsComponent } from './container/featured-brands/featured-br
     ProductComponent,
     FilterComponent,
     ProductDetailComponent,
-    FeaturedBrandsComponent
+    FeaturedBrandsComponent,
+    SetBackground
   ],
   imports: [
     BrowserModule,
